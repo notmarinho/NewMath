6b0b9ce67ee00b77fb550aa4141afb2d2c18b305
Marinho <mateus.marinho@ufrpe.br>
2023-12-01 13:34:52 -0300

    Added Safe Context

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Provider as ReduxProvider} from 'react-redux';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import Navigation from './src/screens';
 import {store} from './src/store/redux';
 
 function App(): JSX.Element {
   return (
-    <ReduxProvider store={store}>
-      <Navigation />
-    </ReduxProvider>
+    <SafeAreaProvider>
+      <ReduxProvider store={store}>
+        <Navigation />
+      </ReduxProvider>
+    </SafeAreaProvider>
   );
 }
 
